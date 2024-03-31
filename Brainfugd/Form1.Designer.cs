7316824d0db6ec861b42394aec6fec36c2b72247
Adam Ben jeddou <mcadambenjeddou@gmail.com>
2024-03-31 04:01:11 +0200

    Aggiungere i file di progetto.

---
+++ Brainfugd/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Brainfugd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcb_Start = new System.Windows.Forms.PictureBox();
+            this.pcb1 = new System.Windows.Forms.PictureBox();
+            this.lbl_OutputLetters = new System.Windows.Forms.Label();
+            this.lbl_OutputNumber = new System.Windows.Forms.Label();
+            this.lbl_Mem1 = new System.Windows.Forms.Label();
+            this.lbl_Mem2 = new System.Windows.Forms.Label();
+            this.lbl_Mem3 = new System.Windows.Forms.Label();
+            this.lbl_Mem4 = new System.Windows.Forms.Label();
+            this.lbl_Mem5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_Start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcb_Start
+            // 
+            this.pcb_Start.BackColor = System.Drawing.Color.Silver;
+            this.pcb_Start.Location = new System.Drawing.Point(130, 320);
+            this.pcb_Start.Name = "pcb_Start";
+            this.pcb_Start.Size = new System.Drawing.Size(50, 50);
+            this.pcb_Start.TabIndex = 0;
+            this.pcb_Start.TabStop = false;
+            // 
+            // pcb1
+            // 
+            this.pcb1.BackColor = System.Drawing.Color.White;
+            this.pcb1.Location = new System.Drawing.Point(185, 330);
+            this.pcb1.Name = "pcb1";
+            this.pcb1.Size = new System.Drawing.Size(50, 50);
+            this.pcb1.TabIndex = 1;
+            this.pcb1.TabStop = false;
+            // 
+            // lbl_OutputLetters
+            // 
+            this.lbl_OutputLetters.AutoSize = true;
+            this.lbl_OutputLetters.Location = new System.Drawing.Point(144, 81);
+            this.lbl_OutputLetters.Name = "lbl_OutputLetters";
+            this.lbl_OutputLetters.Size = new System.Drawing.Size(35, 13);
+            this.lbl_OutputLetters.TabIndex = 2;
+            this.lbl_OutputLetters.Text = "label1";
+            // 
+            // lbl_OutputNumber
+            // 
+            this.lbl_OutputNumber.AutoSize = true;
+            this.lbl_OutputNumber.Location = new System.Drawing.Point(347, 81);
+            this.lbl_OutputNumber.Name = "lbl_OutputNumber";
+            this.lbl_OutputNumber.Size = new System.Drawing.Size(35, 13);
+            this.lbl_OutputNumber.TabIndex = 3;
+            this.lbl_OutputNumber.Text = "label2";
+            // 
+            // lbl_Mem1
+            // 
+            this.lbl_Mem1.AutoSize = true;
+            this.lbl_Mem1.Location = new System.Drawing.Point(144, 186);
+            this.lbl_Mem1.Name = "lbl_Mem1";
+            this.lbl_Mem1.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Mem1.TabIndex = 4;
+            this.lbl_Mem1.Text = "label3";
+            // 
+            // lbl_Mem2
+            // 
+            this.lbl_Mem2.AutoSize = true;
+            this.lbl_Mem2.Location = new System.Drawing.Point(200, 186);
+            this.lbl_Mem2.Name = "lbl_Mem2";
+            this.lbl_Mem2.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Mem2.TabIndex = 5;
+            this.lbl_Mem2.Text = "label4";
+            // 
+            // lbl_Mem3
+            // 
+            this.lbl_Mem3.AutoSize = true;
+            this.lbl_Mem3.Location = new System.Drawing.Point(266, 186);
+            this.lbl_Mem3.Name = "lbl_Mem3";
+            this.lbl_Mem3.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Mem3.TabIndex = 6;
+            this.lbl_Mem3.Text = "label5";
+            // 
+            // lbl_Mem4
+            // 
+            this.lbl_Mem4.AutoSize = true;
+            this.lbl_Mem4.Location = new System.Drawing.Point(333, 186);
+            this.lbl_Mem4.Name = "lbl_Mem4";
+            this.lbl_Mem4.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Mem4.TabIndex = 7;
+            this.lbl_Mem4.Text = "label6";
+            // 
+            // lbl_Mem5
+            // 
+            this.lbl_Mem5.AutoSize = true;
+            this.lbl_Mem5.Location = new System.Drawing.Point(407, 186);
+            this.lbl_Mem5.Name = "lbl_Mem5";
+            this.lbl_Mem5.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Mem5.TabIndex = 8;
+            this.lbl_Mem5.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.lbl_Mem5);
+            this.Controls.Add(this.lbl_Mem4);
+            this.Controls.Add(this.lbl_Mem3);
+            this.Controls.Add(this.lbl_Mem2);
+            this.Controls.Add(this.lbl_Mem1);
+            this.Controls.Add(this.lbl_OutputNumber);
+            this.Controls.Add(this.lbl_OutputLetters);
+            this.Controls.Add(this.pcb1);
+            this.Controls.Add(this.pcb_Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_Start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcb_Start;
+        private System.Windows.Forms.PictureBox pcb1;
+        private System.Windows.Forms.Label lbl_OutputLetters;
+        private System.Windows.Forms.Label lbl_OutputNumber;
+        private System.Windows.Forms.Label lbl_Mem1;
+        private System.Windows.Forms.Label lbl_Mem2;
+        private System.Windows.Forms.Label lbl_Mem3;
+        private System.Windows.Forms.Label lbl_Mem4;
+        private System.Windows.Forms.Label lbl_Mem5;
+    }
+}
+
---
+++ Brainfugd/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Brainfugd
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> Cells = new List<PictureBox>();
+        List<string> CellsType = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            //WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cells.Add(pcb_Start);
+            CellsType.Add("Start");
+            Cells.Add(pcb1);
+            CellsType.Add("Empty");
+            int nCells = (ClientSize.Width - Cells[0].Location.X) / 55 + 1;
+            for(int i = 2; i < nCells; i++)
+            {
+                PictureBox p = new PictureBox()
+                {
+                    Name = "pcb" + Cells.Count,
+                    Location = new Point(Cells[i - 1].Location.X + 55, Cells[i - 1].Location.Y),
+                    Size = new Size(50, 50),
+                    BackColor = Color.White,
+                };
+                Controls.Add(p);
+                Cells.Add(p);
+                CellsType.Add("Empty");
+            }
+        }
+
+        int selectedCell = 0;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right && CellsType[selectedCell] == "End")
+            {
+                Cells[selectedCell].Top += 10;
+                selectedCell = 0;
+                Cells[selectedCell].Top -= 10;
+            }
+            else if (e.KeyCode == Keys.Right && selectedCell < Cells.Count - 1)
+            {
+                NextCell();
+            }
+            if (e.KeyCode == Keys.Left && selectedCell > 0)
+            {
+                PreviousCell();
+            }
+            if (selectedCell != 0)
+            {
+                if (e.KeyCode == Keys.Space || e.KeyCode == Keys.Back)
+                {
+                    Cells[selectedCell].BackColor = Color.White;
+                    CellsType[selectedCell] = "Empty";
+                }
+                if (e.KeyCode == Keys.Oemplus)
+                {
+                    Cells[selectedCell].BackColor = Color.Blue;
+                    CellsType[selectedCell] = "Plus";
+                }
+                if (e.KeyCode == Keys.OemMinus)
+                {
+                    Cells[selectedCell].BackColor = Color.Yellow;
+                    CellsType[selectedCell] = "Minus";
+                }
+                if (e.KeyCode == Keys.Oemcomma)
+                {
+                    Cells[selectedCell].BackColor = Color.Orange;
+                    CellsType[selectedCell] = "Input";
+                }
+                if (e.KeyCode == Keys.OemPeriod)
+                {
+                    Cells[selectedCell].BackColor = Color.Cyan;
+                    CellsType[selectedCell] = "Output";
+                }
+                if (e.KeyCode == Keys.E)
+                {
+                    Cells[selectedCell].BackColor = Color.Purple;
+                    CellsType[selectedCell] = "End";
+                }
+            }
+            if(e.KeyCode == Keys.Enter && selectedCell == 0)
+            {
+                RunProgram();
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (selectedCell != 0)
+            {
+                if (e.KeyChar == '<')
+                {
+                    Cells[selectedCell].BackColor = Color.Green;
+                    CellsType[selectedCell] = "Previous";
+                }
+                if (e.KeyChar == '>')
+                {
+                    Cells[selectedCell].BackColor = Color.Red;
+                    CellsType[selectedCell] = "Next";
+                }
+                if (e.KeyChar == '[')
+                {
+                    Cells[selectedCell].BackColor = Color.Black;
+                    CellsType[selectedCell] = "StartLoop";
+                }
+                if (e.KeyChar == ']')
+                {
+                    Cells[selectedCell].BackColor = Color.DarkGray;
+                    CellsType[selectedCell] = "EndLoop";
+                }
+            }
+        }
+
+        private async void RunProgram()
+        {
+            lbl_OutputLetters.Text = "";
+            lbl_OutputNumber.Text = "";
+            Label[] Memories = new Label[5];
+            int x = 4;
+            foreach(Control c in Controls)
+            {
+                if(c is Label && c != lbl_OutputLetters && c != lbl_OutputNumber)
+                {
+                    c.Text = "00";
+                    Memories[x] = (Label)c;
+                    x--;
+                }
+            }
+            int[] MemoriesValue = {0, 0, 0, 0, 0};
+            int selectedMemory = 0;
+            Memories[0].BackColor = Color.White;
+            for(int i = 1; i < Cells.Count; i++)
+            {
+                switch (CellsType[i])
+                {
+                    case "Start":
+                        break;
+                    case "Empty":
+                        break;
+                    case "Plus":
+                        MemoriesValue[selectedMemory]++;
+                        Memories[selectedMemory].Text = MemoriesValue[selectedMemory].ToString();
+                        break;
+                    case "Minus":
+                        MemoriesValue[selectedMemory]--;
+                        Memories[selectedMemory].Text = MemoriesValue[selectedMemory].ToString();
+                        break;
+                    case "Previous":
+                        Memories[selectedMemory].BackColor = BackColor;
+                        selectedMemory--;
+                        Memories[selectedMemory].BackColor = Color.White;
+                        break;
+                    case "Next":
+                        Memories[selectedMemory].BackColor = BackColor;
+                        selectedMemory++;
+                        Memories[selectedMemory].BackColor = Color.White;
+                        break;
+                    case "Input":
+                        Random rand = new Random();
+                        MemoriesValue[selectedMemory] = rand.Next(1, 99);//
+                        Memories[selectedMemory].Text = MemoriesValue[selectedMemory].ToString();
+                        break;
+                    case "Output":
+                        lbl_OutputNumber.Text = MemoriesValue[selectedMemory].ToString();
+                        lbl_OutputLetters.Text += "A";
+                        break;
+                    case "StartLoop":
+                        break;
+                    case "EndLoop":
+                        if (MemoriesValue[selectedMemory] != 0)
+                        {
+                            while (CellsType[selectedCell] != "StartLoop")
+                            {
+                                PreviousCell();
+                                i--;
+                            }
+                            PreviousCell();
+                            i--;
+                        }
+                        break;
+                    case "End":
+                        NextCell();
+                        return;
+                    default:
+                        break;
+                }
+                NextCell();
+                await Task.Delay(50);
+            }
+        }
+
+        private void NextCell()
+        {
+            Cells[selectedCell].Top += 10;
+            selectedCell++;
+            Cells[selectedCell].Top -= 10;
+        }
+        private void PreviousCell()
+        {
+            Cells[selectedCell].Top += 10;
+            selectedCell--;
+            Cells[selectedCell].Top -= 10;
+        }
+    }
+}
